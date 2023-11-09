245f59b085cba9bd2a965bdc95cc6a44f36a502e
MaLiHa1321 <malihasultana2001@gmail.com>
2023-11-09 22:35:53 +0600

    popular section are added

--- src/Pages/Home/populerMenu/PopularMenu.jsx
+++ src/Pages/Home/populerMenu/PopularMenu.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../component/sectionTitle/SectionTitle';
+import axios from 'axios';
 
 const PopularMenu = () => {
+    const [menu, setMenu] = useState([])
+    useEffect(()=>{
+        axios.get('menu.json')
+        .then(res => setMenu(res.data))
+        .catch(err => console.log(err))
+        
+    },[])
     return (
         <div>
             <SectionTitle heading={"From Our Menu"} subHeading={"Popular Items"}></SectionTitle>
-            
+            <h2>{menu.length}</h2>
+            {
+                menu.map()
+            }
         </div>
     );
 };
