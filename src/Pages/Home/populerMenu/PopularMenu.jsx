f1b425aaf39a8dfeaa61051dcbdca444ce64bc17
MaLiHa1321 <malihasultana2001@gmail.com>
2023-11-10 10:33:52 +0600

    menuItem section are added

--- src/Pages/Home/populerMenu/PopularMenu.jsx
+++ src/Pages/Home/populerMenu/PopularMenu.jsx
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../component/sectionTitle/SectionTitle';
 import axios from 'axios';
+import MenuItem from '../../shared/MenuItem/MenuItem';
 
 const PopularMenu = () => {
     const [menu, setMenu] = useState([])
     useEffect(()=>{
         axios.get('menu.json')
-        .then(res => setMenu(res.data))
+        .then(res => {
+            const popularItem = res.data.filter(item => item.category === 'popular');
+            setMenu(popularItem)
+        } )
         .catch(err => console.log(err))
         
     },[])
     return (
-        <div>
+        <div className='mb-12'>
             <SectionTitle heading={"From Our Menu"} subHeading={"Popular Items"}></SectionTitle>
             <h2>{menu.length}</h2>
-            {
-                menu.map()
-            }
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+                {
+                    menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Pages/shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MenuItem = ({item}) => {
+    // console.log(Object.keys(item).join(','))
+    const {id,name,recipe,image,category,price} = item;
+
+    return (
+        <div className='flex space-x-4'>
+            <img src={image} style={{borderRadius: '0 200px 200px'}} className='w-[100px] object-cover' alt="" />
+            <div>
+                <h3 className='uppercase font-bold'>{name}---------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className='text-yellow-400 font-bold'>${price}</p>
+            
+        </div>
+    );
+};
+
+export default MenuItem;
